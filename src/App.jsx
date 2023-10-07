34171841b03b554f7b53594dae2bd727efb3dad3
metalekan <metalekan@gmail.com>
2023-10-07 13:19:19 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
-import Header from './component/Header';
-import Developer from './component/Developer'
-import Tools from './component/Tools'
-import Projects from './component/Projects'
-import Contact from './component/Contact'
+import Header from "./component/Header";
+import Developer from "./component/Developer";
+import Tools from "./component/Tools";
+import Projects from "./component/Projects";
+import Contact from "./component/Contact";
 
 export const ProjectContext = createContext();
 
 const App = () => {
   const [toggle, setToggle] = useState(false);
+  const [engineStart, setEngineStart] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => { setEngineStart(true) }, 5000)
+  }, [])
+  
   return (
-    <ProjectContext.Provider value={{toggle, setToggle}}>
-      <div className="flex flex-col bg-deepurple text-center">
-        <Header />
-        <Developer />
-        <Tools />
-        <Projects />
-        <Contact />
+    <ProjectContext.Provider value={{ toggle, setToggle }}>
+      <div className="flex flex-col bg-deepurple items-center justify-center text-center min-h-screen">
+      <div className={engineStart ? 'hidden' : 'loader'}></div>
+        <div className={engineStart ? 'flex flex-col' : 'hidden'}>
+          <Header />
+          <Developer />
+          <Tools />
+          <Projects />
+          <Contact />
+        </div>
       </div>
     </ProjectContext.Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
